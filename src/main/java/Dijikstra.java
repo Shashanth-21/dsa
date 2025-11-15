f4d407fe363d4e6875864cc75053e19800667968
Shashanth-21 <shashanth21112001@gmail.com>
2025-11-15 23:22:48 +0530

    test commit

--- src/main/java/Dijikstra.java
+++ src/main/java/Dijikstra.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Dijikstra {
+    public static void main(String[] args) {
+        List<Integer>[]arr = new List[20];
+        for(int i = 0; i < 20; i++){
+            arr[i] = new ArrayList<>();
+        }
+    }
 }
